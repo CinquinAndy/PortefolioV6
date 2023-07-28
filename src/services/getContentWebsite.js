8c284ce775ac058b16ac1140e2192957167f3c37
Cinquin Andy <andy.cinquin@infotel.com>
2023-07-28 13:48:57 +0200

    🔧 fix(getContentWebsite.js): change variable name from `data_content_website` to `dataContentWebsite` 🔧 fix(getContentWebsite.js): change variable name from `processedContentRealisations` to `processedContentArticles` ✨ feat(getContentWebsite.js): add JSDoc comments to functions `fetchAPI`, `processMarkdown`, and `updateNestedProperty` ✨ feat(getContentWebsite.js): add functions `getServices`, `getRealisations`, `getArticles`, `getRealisationBySlug`, `getArticleBySlug`, `processRealisationData`, `processArticleData`, `getRealisationPaths`, and `getArticlePaths`

--- src/services/getContentWebsite.js
+++ src/services/getContentWebsite.js
@@ -1,6 +1,12 @@
 import { remark } from 'remark'
 import html from 'remark-html'
 
+/**
+ * Fetch data from API
+ * @param path
+ * @param options
+ * @returns {Promise<{notFound: boolean}|any>}
+ */
 export async function fetchAPI(path, options = {}) {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${path}`, {
 		method: 'GET',
@@ -19,11 +25,23 @@ export async function fetchAPI(path, options = {}) {
 	return await res.json()
 }
 
+/**
+ * Process markdown content
+ * @param markdownContent
+ * @returns {Promise<string>}
+ */
 export async function processMarkdown(markdownContent) {
 	const result = await remark().use(html).process(markdownContent)
 	return result.toString()
 }
 
+/**
+ * Update nested property
+ * @param obj
+ * @param path
+ * @param newValue
+ * @returns {*}
+ */
 export function updateNestedProperty(obj, path, newValue) {
 	// Convertir le chemin en tableau de clés
 	const keys = Array.isArray(path) ? path : path.split('.')
@@ -44,6 +62,10 @@ export function updateNestedProperty(obj, path, newValue) {
 	return newObj
 }
 
+/**
+ * Get content website
+ * @returns {Promise<*>}
+ */
 export async function getContentWebsite() {
 	const data_content_website = await fetchAPI(
 		'api/content-website?populate=deep'
@@ -67,30 +89,61 @@ export async function getContentWebsite() {
 	)
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/**
+ * Get services
+ * @returns {Promise<{notFound: boolean}|*>}
+ */
 export async function getServices() {
 	return await fetchAPI('api/services?populate=deep')
 }
 
+/**
+ * Get realisations
+ * @returns {Promise<{notFound: boolean}|*>}
+ */
 export async function getRealisations() {
 	return await fetchAPI('api/realisations?populate=deep,2&sort=rank')
 }
 
+/**
+ * Get articles
+ * @returns {Promise<{notFound: boolean}|*>}
+ */
 export async function getArticles() {
 	return await fetchAPI('api/articles?populate=deep,2&sort=rank')
 }
 
+/**
+ * Get realisation by slug
+ * @param slug
+ * @returns {Promise<{notFound: boolean}|*>}
+ */
 export async function getRealisationBySlug(slug) {
 	return fetchAPI(
 		`api/realisations?populate=deep,3&sort=rank&filters[slug][$eq]=${slug}`
 	)
 }
 
+/**
+ * Get article by slug
+ * @param slug
+ * @returns {Promise<{notFound: boolean}|*>}
+ */
 export async function getArticleBySlug(slug) {
 	return fetchAPI(
 		`api/articles?populate=deep,3&sort=rank&filters[slug][$eq]=${slug}`
 	)
 }
 
+/**
+ * Get article by slug
+ * @param realisationData
+ * @returns {Promise<*&{data: {attributes: (*&{content: string})}}>}
+ */
 export async function processRealisationData(realisationData) {
 	const processedContentRealisations = await processMarkdown(
 		realisationData.data[0].attributes.content
@@ -107,8 +160,49 @@ export async function processRealisationData(realisationData) {
 	}
 }
 
+/**
+ * Get article by slug
+ * @param articleData
+ * @returns {Promise<*&{data: {attributes: (*&{content: string})}}>}
+ */
+export async function processArticleData(articleData) {
+	const processedContentArticles = await processMarkdown(
+		articleData.data[0].attributes.content
+	)
+	return {
+		...articleData,
+		data: {
+			// ...articleData.data,
+			attributes: {
+				...articleData.data[0].attributes,
+				content: processedContentArticles.toString(),
+			},
+		},
+	}
+}
+
+/**
+ * Get realisation paths
+ * @returns {Promise<*|*[]>}
+ */
 export async function getRealisationPaths() {
 	const data = await getRealisations()
+
+	return (
+		data?.data?.map(record => ({
+			params: {
+				slug: record.attributes.slug,
+			},
+		})) || []
+	)
+}
+
+/**
+ * Get article paths
+ * @returns {Promise<*|*[]>}
+ */
+export async function getArticlePaths() {
+	const data = await getArticles()
 
 	return (
 		data?.data?.map(record => ({
