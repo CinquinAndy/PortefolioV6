fc90be3db600fbe5a1d1ed9ba0d327c0d3dc0b48
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-24 09:43:05 +0100

    feat: Add server-side rendering for generating sitemap
    This commit adds server-side rendering functionality to generate a sitemap.xml file. The code includes the following significant changes:
    - Import React and necessary modules
    - Define a functional component `Sitemap`
    - Implement `getServerSideProps` function to fetch data and generate the sitemap XML content
    - Create helper functions `getFilesFromDir` and `filterAndFormatPaths` for retrieving files from directories and filtering paths respectively
    - Fetch articles and portfolio data from APIs to generate dynamic paths for blog posts and portfolio items
    - Generate the sitemap XML content with URLs, last modification date, change frequency, and priority values
    - Set the response header as 'Content-Type' of 'text/xml' and write the generated sitemap content to the response body
    
    The generated sitemap will include static paths, dynamic paths for blog posts and portfolio items, as well as alternative URLs.

---
+++ src/app/sitemap.xml/page.js
@@ -0,0 +1,171 @@
+import React from 'react'
+import { readdirSync, lstatSync } from 'fs'
+import * as path from 'path'
+
+const Sitemap = () => {
+    return null
+}
+
+export const getServerSideProps = async ({ res }) => {
+    // Function to get files from a directory recursively
+    function getFilesFromDir(startPath) {
+        let results = []
+
+        function finder(startPath) {
+            let files = readdirSync(startPath)
+
+            for (const element of files) {
+                let filename = path.join(startPath, element)
+                let stat = lstatSync(filename)
+
+                if (stat.isDirectory()) {
+                    finder(filename) // recurse
+                } else {
+                    results.push(filename)
+                }
+            }
+        }
+
+        finder(startPath)
+
+        return results
+    }
+
+    // Common function to filter and format paths for both URLs
+    function filterAndFormatPaths(staticPaths, baseUrl) {
+        return staticPaths.filter(item => {
+            return ![
+                `${baseUrl}/api/sendMail`,
+                `${baseUrl}/blog/[slug]`,
+                `${baseUrl}/portefolio/[slug]`,
+            ].includes(item)
+        })
+    }
+
+    let files = getFilesFromDir('./src/pages')
+
+    let staticPaths = files
+        .filter(staticPagePath => {
+            let base = path.basename(staticPagePath)
+            return ![
+                'api',
+                'api/sendMail',
+                '_app.js',
+                '_document.js',
+                '404.js',
+                'sitemap.xml.js',
+                'index.js',
+            ].includes(base)
+        })
+        .map(staticPagePath => {
+            let parsedPath = path.parse(staticPagePath)
+            let newPath = `${parsedPath.dir}/${parsedPath.name}`
+                .replace('src\\pages\\', '')
+                .replace('src\\pages', '')
+                .replace('src/pages', '')
+            return `${process.env.NEXT_PUBLIC_URL}/${newPath}`.replace(
+                `${process.env.NEXT_PUBLIC_URL}//`,
+                `${process.env.NEXT_PUBLIC_URL}/`
+            )
+        })
+
+    let staticPathsAltCopy = JSON.parse(JSON.stringify(staticPaths))
+    // Separate static paths for the alternative URL
+    let staticPathsAlt = staticPathsAltCopy.map(staticPath => {
+        return staticPath.replace(
+            `${process.env.NEXT_PUBLIC_URL}/`,
+            `${process.env.NEXT_PUBLIC_URL_ALT}/`
+        )
+    })
+
+    staticPaths = filterAndFormatPaths(staticPaths, process.env.NEXT_PUBLIC_URL)
+    staticPathsAlt = filterAndFormatPaths(
+        staticPathsAlt,
+        process.env.NEXT_PUBLIC_URL_ALT
+    )
+
+    // get all article for dynamic paths
+    const resultBlog = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/articles`,
+        {
+            method: 'GET',
+            headers: {
+                // 	token
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+            },
+        }
+    ).then(res => res.json())
+
+    const pathsBlog = resultBlog?.data?.map(record => {
+        return `${process.env.NEXT_PUBLIC_URL}/blog/${record.attributes.slug}`
+    })
+
+    // copy of the paths for the alternative URL
+    const resultBlogAlt = JSON.parse(JSON.stringify(resultBlog))
+
+    const pathsBlogAlt = resultBlogAlt?.data?.map(record => {
+        return `${process.env.NEXT_PUBLIC_URL_ALT}/blog/${record.attributes.slug}`
+    })
+
+    // get all article for dynamic paths
+    const resultPortefolio = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/realisations`,
+        {
+            method: 'GET',
+            headers: {
+                // 	token
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+            },
+        }
+    ).then(res => res.json())
+
+    const pathsPortefolio = resultPortefolio?.data?.map(record => {
+        return `${process.env.NEXT_PUBLIC_URL}/portefolio/${record.attributes.slug}`
+    })
+
+    const resultPortefolioAlt = JSON.parse(JSON.stringify(resultPortefolio))
+
+    const pathsPortefolioAlt = resultPortefolioAlt?.data?.map(record => {
+        return `${process.env.NEXT_PUBLIC_URL_ALT}/portefolio/${record.attributes.slug}`
+    })
+
+    const allPaths = [
+        ...staticPaths,
+        ...pathsBlog,
+        ...pathsPortefolio,
+        ...staticPathsAlt,
+        ...pathsBlogAlt,
+        ...pathsPortefolioAlt,
+    ]
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${allPaths
+        .map(url => {
+            return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>weekly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `
+        })
+        .join('')}
+    </urlset>
+  `
+
+    res.setHeader('Content-Type', 'text/xml')
+    res.write(sitemap)
+    res.end()
+
+    return {
+        props: {},
+    }
+}
+
+export default Sitemap
