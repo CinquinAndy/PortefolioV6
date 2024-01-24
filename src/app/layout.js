494d6f2dbd2770244d2d671524413275a1fd865d
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-24 11:05:37 +0100

    feat: Add Google Analytics and update font imports
    - Import `GoogleAnalytics` from `@next/third-parties/google`
    - Update font imports to use `Be_Vietnam_Pro` and `Noto_Serif` from `next/font/google`
    - Remove unused imports (`Head`, `Script`, `Html`)
    - Add import for `getContentWebsite` from "@/services/getContentWebsite"
    - Modify the function signature of `generateMetadata` to accept a locale parameter
    - Update the implementation of `generateMetadata` to call `getContentWebsite(locale)` instead of calling an undefined function
    - Remove unnecessary console.log statement
    - Simplify the structure of the RootLayout component by removing `<Head>` and `<Html>` tags, and moving their contents directly into the body tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import {GoogleAnalytics} from '@next/third-parties/google'
 import '@/styles/btn.css'
 import '@/styles/carroussel.css'
 import '@/styles/distorsions.css'
@@ -6,10 +7,9 @@ import '@/styles/nav.css'
 import 'react-toastify/dist/ReactToastify.css'
 
 import {Be_Vietnam_Pro, Noto_Serif} from 'next/font/google'
-import Head from 'next/head'
 import {ToastContainer} from 'react-toastify'
-import Script from "next/script";
-import {Html, Main, NextScript} from "next/document";
+import {NextScript} from "next/document";
+import {getContentWebsite} from "@/services/getContentWebsite";
 
 const be_vietnam_pro = Be_Vietnam_Pro({
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -25,9 +25,11 @@ const noto_serif = Noto_Serif({
     style: ['normal', 'italic'],
 })
 
-export async function generateMetadata() {
+export async function generateMetadata({locale}) {
     // fetch data
-    const content_website = await getAllContentWebsite()
+    const content_website = await getContentWebsite(locale)
+    console.log("locale", locale)
+
 
     return {
         title:
@@ -44,49 +46,26 @@ export async function generateMetadata() {
                 'en-US': 'https://andy-cinquin.com',
             },
         },
-        icons:{
+        icons: {
             icon: `${process.env.NEXT_PUBLIC_URL}/favicon.webp`
         }
     }
 }
 
 
-export default async function RootLayout({ children }) {
+export default async function RootLayout({children}) {
     return (
         <>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href={`${process.env.NEXT_PUBLIC_URL}/favicon.webp`}/>
-                <Script
-                    strategy={'afterInteractive'}
-                    src="https://www.googletagmanager.com/gtag/js?id=UA-150969790-2"
-                />
-                <Script id={'analytics'} strategy="afterInteractive">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'UA-150969790-2');
-                    `}
-                </Script>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin={'true'}
-                />
-
-            </Head>
-            <Html>
-                <body
-                    className={`relative bg-gradient-to-b from-indigo-1100 to-sky-1100 text-slate-50`}
-                >
-                <Main/>
-                <NextScript/>
-                <ToastContainer/>
-                </body>
-            </Html>
+            <html className={`${be_vietnam_pro.variable} ${noto_serif.variable}`}>
+            <GoogleAnalytics gaId="UA-150969790-2"/>
+            <ToastContainer/>
+            <NextScript/>
+            <body
+                className={`relative bg-gradient-to-b from-indigo-1100 to-sky-1100 text-slate-50`}
+            >
+            {children}
+            </body>
+            </html>
         </>
     )
 }
