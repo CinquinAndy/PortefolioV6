201a1e298d658bdb5c0e07752a2f62a96f83f143
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 00:19:54 +0100

    🔧 Fix: Update import statement in Nav.js
    The import statement for `useRouter` has been updated to use `next/navigation` instead of `next/router`. This change ensures compatibility with the latest version of Next.js. Additionally, the switchLanguage function has been modified to use the updated router.push syntax.

--- src/components/Global/Nav.js
+++ src/components/Global/Nav.js
@@ -1,21 +1,20 @@
+'use client'
 import React, { useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 function Nav({ content_website, selectedMenu, h1, isHome = true }) {
 	const [open, setOpen] = useState(false)
 	const router = useRouter()
-	const { pathname, asPath, query, locale } = router
+	const { asPath, query } = router
 
 	content_website = content_website.attributes
 	const menu = content_website.menu
 	const socials = content_website.socials
 
 	const switchLangage = () => {
-		router.push({ pathname, query }, asPath, {
-			locale: locale === 'fr' ? 'en' : 'fr',
-		})
+		router.push(query, asPath)
 	}
 
 	return (
