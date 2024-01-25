086efdda641fefe51134fdf7ecd5ac0e4c3f5254
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 00:19:49 +0100

    🚀 Refactor code for layout and page components
    - Refactored the font imports in the layout component to use destructuring assignment
    - Removed the unused `generateStaticParams` function from the layout component
    - Cleaned up the code formatting in the layout component
    - Added CSS classes and SVG filters to the body element in the layout component
    - Updated the HTML lang attribute dynamically based on `params.lang` in the layout component
    - Removed console.log statement from the layout component
    - Added CSS classes, imported stylesheets, and fetched data in the page component
    - Updated metadata generation logic to fetch data from API and set appropriate values for title, description, canonical URL, and language alternates

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -5,45 +5,40 @@ import {Noto_Serif_Display, Be_Vietnam_Pro} from 'next/font/google'
 
 const noto_serif_display = Noto_Serif_Display({subsets: ['latin'], variable: '--font-be-vietnam-pro'})
 const be_vietnam_pro = Be_Vietnam_Pro({
-    // weight: "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900"
-    weight: ['100','200','300','400','500','600','700','800','900'],
-    variable: '--font-noto-serif-display',
-    style: ['italic', 'normal'],
-    subsets: ['latin']}
+        weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+        variable: '--font-noto-serif-display',
+        style: ['italic', 'normal'],
+        subsets: ['latin']
+    }
 )
 
-export async function generateStaticParams() {
-    return [{lang: 'en'}, {lang: 'fr'}]
-}
-
 export default function RootLayout({children, params}) {
-    console.log("inside server", params.lang);
     return (
         <html lang={`${params.lang}`}>
-            <ToastContainer/>
-            <GoogleAnalytics gaId="UA-150969790-2"/>
-            <body className={`relative text-slate-50 ${noto_serif_display.variable} ${be_vietnam_pro.variable}`}
-            >
-                <div className="gradient-bg">
-                    <svg xmlns="http://www.w3.org/2000/svg" className={"gradient_svg"}>
-                        <defs>
-                            <filter id="goo">
-                                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"/>
-                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo"/>
-                                <feBlend in="SourceGraphic" in2="goo"/>
-                            </filter>
-                        </defs>
-                    </svg>
-                    <div className="gradients-container">
-                        <div className="g1"></div>
-                        <div className="g2"></div>
-                        <div className="g3"></div>
-                        <div className="g4"></div>
-                        <div className="g5"></div>
-                    </div>
-                </div>
-                {children}
-            </body>
+        <body className={`relative text-slate-50 ${noto_serif_display.variable} ${be_vietnam_pro.variable}`}
+        >
+        <ToastContainer/>
+        <GoogleAnalytics gaId="UA-150969790-2"/>
+        <div className="gradient-bg">
+            <svg xmlns="http://www.w3.org/2000/svg" className={"gradient_svg"}>
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"/>
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo"/>
+                        <feBlend in="SourceGraphic" in2="goo"/>
+                    </filter>
+                </defs>
+            </svg>
+            <div className="gradients-container">
+                <div className="g1"></div>
+                <div className="g2"></div>
+                <div className="g3"></div>
+                <div className="g4"></div>
+                <div className="g5"></div>
+            </div>
+        </div>
+        {children}
+        </body>
         </html>
     )
 }
--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,9 +1,99 @@
-export const metadata = {
-    title: 'My Page Title',
+import '@/styles/btn.css'
+import '@/styles/carroussel.css'
+import '@/styles/distorsions.css'
+import '@/styles/main.css'
+import '@/styles/nav.css'
+import 'react-toastify/dist/ReactToastify.css'
+import {getArticles, getContentWebsite, getRealisations, getServices} from "@/services/getContentWebsite";
+import Nav from "@/components/Global/Nav";
+import Image from "next/image";
+import Services from "@/components/Global/Services";
+import Realisations from "@/components/Global/Realisations";
+import Articles from "@/components/Global/Articles";
+import Cta from "@/components/Global/Cta";
+import Footer from "@/components/Global/Footer";
+
+export async function generateMetadata({params}) {
+    // fetch data
+    const content_website = await getContentWebsite(params.lang)
+
+    return {
+        title:
+            content_website?.data?.attributes?.content_home?.seo?.title ||
+            'Andy Cinquin - Freelance Entrepreneur & Developer',
+        description:
+            content_website?.data?.attributes?.content_home?.seo?.description ||
+            "Professional portfolio of Andy Cinquin, freelance software developer, Nantes and surrounding areas. Custom development, web, applications",
+        metadataBase: new URL(`https://andy-cinquin.fr`),
+        alternates: {
+            canonical: content_website?.data?.attributes?.content_home?.seo?.canonical || '/',
+            languages: {
+                'fr-FR': '/',
+                'en-US': 'https://andy-cinquin.com',
+            },
+        },
+    }
 }
 
-export default function Page({params}) {
+export default async function Page({params}) {
+    const content_website = await getContentWebsite(params.lang)
+    const services = await getServices(params.lang)
+    const realisations = await getRealisations(params.lang)
+    const articles = await getArticles(params.lang)
+
     return <>
-        page -- {params.lang}
+        <Nav content_website={content_website.data}/>
+
+        <div className={"w-screen h-screen absolute top-0 left-0 -z-10 mask"}>
+            <div className="absolute left-1/2 top-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 transform items-center justify-center mask">
+                <div className="video-background relative clear-both m-0 h-[100vh] w-[100vw] max-w-[100vw] overflow-x-hidden p-0 mask">
+                    <Image
+                        src={'/assets/images/bg_opti.webp'}
+                        alt={'bg_opti'}
+                        className={
+                            'absolute left-0 top-0 blur-md  ' +
+                            'mix-difference -z-10 block bg-slate-900 object-cover opacity-75 mask  '
+                        }
+                        quality={10}
+                        fill={true}
+                        loading="eager"
+                    />
+
+                    <iframe
+                        title="video"
+                        className={
+                            'absolute left-0 top-0 h-full w-full object-cover object-center mask ' +
+                            'mix-difference animate-video -z-10 block bg-slate-900 object-cover opacity-75'
+                        }
+                        id="topHeroVideo"
+                        src="https://player.vimeo.com/video/905713566?background=1"
+                        width="1920"
+                        height="1080"
+                        frameBorder="0"
+                        allow="autoplay"
+                        data-ready="true"
+                        allowFullScreen
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div className={"relative"}>
+            <Services content_website={content_website.data} services={services.data}/>
+            <Realisations
+                content_website={content_website.data}
+                realisations={realisations.data}
+                slice={3}
+                isHome={true}
+            />
+            <Articles
+                content_website={content_website.data}
+                articles={articles.data}
+                slice={3}
+                isHome={true}
+            ></Articles>
+            <Cta content_website={content_website.data}/>
+        </div>
+        <Footer content_website={content_website.data}/>
     </>
 }
