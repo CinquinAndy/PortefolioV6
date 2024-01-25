68265f80211bb8ef5381e4549e5a17a8069f4aea
CinquinAndy <cinquin.andy@gmail.com>
2024-01-25 09:58:32 +0100

    add background

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,10 +32,29 @@ export default function Document() {
 			</Head>
 			{/* if the page is 404 then, set overflow to hidden */}
 			<body
-				className={`relative bg-gradient-to-b from-indigo-1100 to-sky-1100 text-slate-50`}
+				// className={`relative bg-gradient-to-b from-indigo-1100 to-sky-1100 text-slate-50`}
+				className={`relative text-slate-50`}
 			>
-				<Main />
-				<NextScript />
+			<div className="gradient-bg">
+				<svg xmlns="http://www.w3.org/2000/svg" className={"gradient_svg"}>
+					<defs>
+						<filter id="goo">
+							<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"/>
+							<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo"/>
+							<feBlend in="SourceGraphic" in2="goo"/>
+						</filter>
+					</defs>
+				</svg>
+				<div className="gradients-container">
+					<div className="g1"></div>
+					<div className="g2"></div>
+					<div className="g3"></div>
+					<div className="g4"></div>
+					<div className="g5"></div>
+				</div>
+			</div>
+			<Main/>
+			<NextScript/>
 			</body>
 		</Html>
 	)
