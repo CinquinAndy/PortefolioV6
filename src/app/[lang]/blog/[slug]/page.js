557b559af376165b38c972b69cf50d9bbff0e843
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 20:11:04 +0100

    🔧 Refactor page.js: Remove unnecessary code and add state for handling click event
    This commit refactors the `page.js` file by removing unnecessary code related to fetching data. It also adds a state variable `open` and a function `handleClick` to handle the click event. These changes improve the readability and maintainability of the code.

--- src/app/[lang]/blog/[slug]/page.js
+++ src/app/[lang]/blog/[slug]/page.js
@@ -16,55 +16,58 @@ import Link from "next/link";
 
 export async function generateMetadata({ params }) {
 	// fetch data
-	let content_website = await getContentWebsite(params.lang)
-	content_website = content_website?.data
 	let article = await getArticleBySlug(params.slug, params.lang)
-	article = article?.data
 
 	let processedArticle = await processArticleData(article)
 	processedArticle = processedArticle?.data
 
 	return {
 		title:
-			article?.attributes?.seo_title ||
+			processedArticle?.attributes?.seo_title ||
 			'Andy Cinquin - Freelance Entrepreneur & Developer',
 		description:
-			article?.attributes?.seo_description ||
+			processedArticle?.attributes?.seo_description ||
 			'Professional portfolio of Andy Cinquin, freelance software developer, Nantes and surrounding areas. Custom development, web, applications',
 		metadataBase: new URL(`https://andy-cinquin.com`),
 		alternates: {
-			canonical: article?.attributes?.seo_canonical || '/',
+			canonical: processedArticle?.attributes?.seo_canonical || '/',
 			languages: {
 				'en-US': `${
 					params.locale === 'fr'
 						? process.env.NEXT_PUBLIC_URL_ALT
 						: process.env.NEXT_PUBLIC_URL
-				}/blog/${article?.attributes?.slug}`,
+				}/blog/${processedArticle?.attributes?.slug}`,
 				'fr-FR': `${
 					params.locale === 'fr'
 						? process.env.NEXT_PUBLIC_URL_ALT
 						: process.env.NEXT_PUBLIC_URL
-				}/blog/${article?.attributes?.slug}`,
+				}/blog/${processedArticle?.attributes?.slug}`,
 			},
 		},
 	}
 }
 
 export default async function Page({ params }) {
+	// fetch data
 	let content_website = await getContentWebsite(params.lang)
 	content_website = content_website?.data
 	let article = await getArticleBySlug(params.slug, params.lang)
-	article = article?.data
 
 	let processedArticle = await processArticleData(article)
 	processedArticle = processedArticle?.data
+
+	const [open, setOpen] = useState(false)
+
+	const handleClick = () => {
+		setOpen(!open)
+	}
 
 	return (
 		<>
 			<Nav
 				content_website={content_website}
 				isHome={false}
-				h1={articles?.attributes?.title}
+				h1={processedArticle?.attributes?.title}
 			/>
 			<div>
 				<div className={'relative'}>
@@ -97,7 +100,7 @@ export default async function Page({ params }) {
 									/>
 								</div>
 								<div className={'flex flex-col gap-4'}>
-									{article?.attributes?.links?.map((link, index) => {
+									{processedArticle?.attributes?.links?.map((link, index) => {
 										return (
 											<div key={index} className={'flex'}>
 												<Link
@@ -135,7 +138,7 @@ export default async function Page({ params }) {
 								<div
 									className={'prose prose-invert my-8 [&>*]:!decoration-auto'}
 								>
-									<Layout value={article?.attributes?.content.toString()} />
+									<Layout value={processedArticle?.attributes?.content.toString()} />
 								</div>
 							</article>
 						</div>
@@ -147,180 +150,3 @@ export default async function Page({ params }) {
 		</>
 	)
 }
-
-// import React from 'react'
-// import Nav from '@/components/Global/Nav'
-// import Footer from '@/components/Global/Footer'
-// import Head from 'next/head'
-// import { useRouter } from 'next/router'
-// import { Layout } from '@/components/Global/Layout'
-// import Cta from '@/components/Global/Cta'
-// import {
-// 	getArticleBySlug,
-// 	getArticlePaths,
-// 	getContentWebsite,
-// 	processArticleData,
-// } from '@/services/getContentWebsite'
-// import { replaceTitle } from '@/services/utils'
-// import { LinkIcon } from '@heroicons/react/20/solid'
-// import Link from 'next/link'
-//
-// /**
-//  * @param props
-//  * @constructor
-//  */
-// function Blog({ content_website, articles }) {
-// 	let router = useRouter()
-// 	const { locale } = router
-//
-// 	const [open, setOpen] = React.useState(false)
-//
-// 	const handleClick = () => {
-// 		setOpen(!open)
-// 	}
-// 	return (
-// 		<>
-// 			<Head>
-// 				<title>{articles?.attributes?.seo_title}</title>
-// 				<meta
-// 					name="description"
-// 					content={articles?.attributes?.seo_description}
-// 				/>
-// 				{/*	seo tag canonical link */}
-// 				<link
-// 					rel="canonical"
-// 					href={`${
-// 						locale === 'fr'
-// 							? process.env.NEXT_PUBLIC_URL_ALT
-// 							: process.env.NEXT_PUBLIC_URL
-// 					}/blog/${articles?.attributes?.slug}`}
-// 				/>
-// 				<link
-// 					rel="alternate"
-// 					href={`${
-// 						locale === 'fr'
-// 							? process.env.NEXT_PUBLIC_URL_ALT
-// 							: process.env.NEXT_PUBLIC_URL
-// 					}/blog/${articles?.attributes?.slug}`}
-// 					hrefLang={locale}
-// 				/>
-// 			</Head>
-//
-// 			<Nav
-// 				content_website={content_website}
-// 				isHome={false}
-// 				h1={articles?.attributes?.title}
-// 			/>
-// 			<div>
-// 				<div className={'relative'}>
-// 					<div
-// 						className={
-// 							'my-24 grid grid-cols-1 gap-[100px] px-6 md:my-48 md:px-16 2xl:px-0'
-// 						}
-// 					>
-// 						<div
-// 							className={
-// 								'shadow-innercustom relative mx-auto max-w-5xl cursor-pointer p-8 md:col-span-2 md:p-20 xl:max-w-7xl'
-// 							}
-// 						>
-// 							<div
-// 								className={
-// 									'flex w-full items-center justify-evenly gap-4 md:gap-8'
-// 								}
-// 							>
-// 								<div className={'flex items-center gap-2'}>
-// 									<h2
-// 										className={
-// 											'text-md font-black md:text-3xl [&>*]:font-black'
-// 										}
-// 										dangerouslySetInnerHTML={{
-// 											__html: replaceTitle(
-// 												content_website?.attributes?.content_realisations
-// 													?.title_links
-// 											),
-// 										}}
-// 									/>
-// 								</div>
-// 								<div className={'flex flex-col gap-4'}>
-// 									{articles?.attributes?.links?.map((link, index) => {
-// 										return (
-// 											<div key={index} className={'flex'}>
-// 												<Link
-// 													key={link?.id}
-// 													href={link?.url}
-// 													rel={'noopener noreferrer'}
-// 													className={
-// 														'custom-button-icons relative flex items-center gap-4 rounded border border-indigo-600 bg-transparent px-6 py-2 text-xs xl:px-8 xl:py-2 xl:text-sm'
-// 													}
-// 												>
-// 													{link?.label}
-// 													<LinkIcon
-// 														className={
-// 															'absolute -right-2 -top-2 h-4 w-4 rotate-6'
-// 														}
-// 													/>
-// 												</Link>
-// 											</div>
-// 										)
-// 									})}
-// 								</div>
-// 							</div>
-// 						</div>
-//
-// 						<div className="mx-auto max-w-7xl">
-// 							<h2
-// 								className={'text-lg font-black md:text-3xl [&>*]:font-black'}
-// 								dangerouslySetInnerHTML={{
-// 									__html: replaceTitle(
-// 										content_website?.attributes?.content_blog?.title_content
-// 									),
-// 								}}
-// 							/>
-// 							<article>
-// 								<div
-// 									className={'prose prose-invert my-8 [&>*]:!decoration-auto'}
-// 								>
-// 									<Layout value={articles?.attributes?.content.toString()} />
-// 								</div>
-// 							</article>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</div>
-// 			<Cta content_website={content_website} />
-// 			<Footer content_website={content_website} />
-// 		</>
-// 	)
-// }
-//
-// export default Blog
-//
-// export async function getStaticPaths({ locale }) {
-// 	const paths = await getArticlePaths(locale)
-//
-// 	return {
-// 		paths,
-// 		fallback: 'blocking',
-// 	}
-// }
-//
-// export async function getStaticProps({ params, locale }) {
-// 	const content_website = await getContentWebsite(locale)
-// 	const articles = await getArticleBySlug(params.slug, locale)
-//
-// 	if (!content_website || !articles) {
-// 		return {
-// 			notFound: true,
-// 		}
-// 	}
-//
-// 	const processedArticles = await processArticleData(articles)
-//
-// 	return {
-// 		props: {
-// 			content_website: content_website.data,
-// 			articles: processedArticles.data,
-// 		},
-// 		revalidate: 10,
-// 	}
-// }
