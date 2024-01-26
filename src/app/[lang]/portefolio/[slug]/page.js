b1f9f2f488530a49a3bc597f6ca0377cdbaf9570
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 20:37:12 +0100

    🔧 Refactor GalerySection component and import it in page.js
    This commit refactors the GalerySection component by moving its code from the page.js file to a separate file called GalerySection.js. The GalerySection component is then imported into the page.js file, improving code organization and separation of concerns.

--- src/app/[lang]/portefolio/[slug]/page.js
+++ src/app/[lang]/portefolio/[slug]/page.js
@@ -1,8 +1,6 @@
 import {
-	getArticleBySlug,
 	getContentWebsite,
 	getRealisationBySlug,
-	processArticleData,
 	processRealisationData,
 } from '@/services/getContentWebsite'
 import Nav from '@/components/Global/Nav'
@@ -12,6 +10,7 @@ import { Layout } from '@/components/Global/Layout'
 import { replaceTitle } from '@/services/utils'
 import { LinkIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
+import { GalerySection } from '@/components/Global/GalerySection'
 
 export async function generateMetadata({ params }) {
 	// fetch data
@@ -61,56 +60,10 @@ export default async function Page({ params }) {
 							'my-24 grid grid-cols-1 gap-[100px] px-6 md:my-48 md:grid-cols-2 2xl:px-0'
 						}
 					>
-						<div
-							onClick={handleClick}
-							className={
-								'shadow-innercustom relative mx-auto max-w-5xl cursor-pointer p-8 md:col-span-2 md:p-20'
-							}
-						>
-							<div className={'flex w-full items-start gap-4 md:gap-8'}>
-								<div className={'flex items-center gap-2'}>
-									<h2
-										className={
-											'text-md font-black md:text-3xl [&>*]:font-black'
-										}
-										dangerouslySetInnerHTML={{
-											__html: replaceTitle(
-												content_website?.attributes?.content_realisations
-													?.title_galery
-											),
-										}}
-									/>
-									<div className={'hidden h-full items-center md:flex'}>
-										<ChevronRightIcon className={'h-6 w-6 md:h-8 md:w-8'} />
-									</div>
-								</div>
-								<button
-									onClick={() => {
-										handleClick()
-									}}
-									className={
-										'custom-button-icons relative flex items-center gap-4 rounded border border-indigo-600 bg-transparent px-6 py-2 text-xs xl:px-8 xl:py-2 xl:text-sm'
-									}
-								>
-									{
-										content_website?.attributes?.content_realisations
-											?.btn_galery?.label
-									}
-									<CameraIcon
-										className={'absolute -right-2 -top-2 h-4 w-4 rotate-6'}
-									/>
-								</button>
-							</div>
-							<Galery
-								handleClick={handleClick}
-								open={open}
-								galery={realisations?.attributes?.galery?.data}
-								title_galery={
-									content_website?.attributes?.content_realisations
-										?.title_galery
-								}
-							/>
-						</div>
+						<GalerySection
+							content_website={content_website}
+							processedRealisation={processedRealisation}
+						/>
 
 						<div className="mx-auto max-w-3xl md:pl-20">
 							<h2
