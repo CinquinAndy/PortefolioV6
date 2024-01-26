1de7612545ca74d956155464f31a92b9e3e988a8
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 23:04:52 +0100

    🔧 Add AOS and its CSS to layout.js
    This commit adds the import statement for AOS library and its corresponding CSS file to the layout.js file. This will allow us to use AOS animations in our application.

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import '@/styles/distorsions.css'
 import '@/styles/main.css'
 import '@/styles/nav.css'
 import 'react-toastify/dist/ReactToastify.css'
+import 'aos/dist/aos.css'
 
 export default function RootLayout({ children }) {
 	return <>{children}</>
