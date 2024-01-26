7e641956b895d93fc6f1550bcd67ba3c54e9ace8
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 21:46:33 +0100

    🔧 Refactor layout and not-found components
    - Updated import statements for GoogleAnalytics, ToastContainer, Noto_Serif_Display, and Be_Vietnam_Pro
    - Reorganized code structure in the RootLayout component
    - Removed unused code from the RootLayout component
    - Added HTML structure to the NotFound component
    - Updated import statements for GoogleAnalytics, ToastContainer, Noto_Serif_Display, and Be_Vietnam_Pro in the NotFound component

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -1,51 +1,55 @@
-import '@/styles/btn.css'
-import '@/styles/carroussel.css'
-import '@/styles/distorsions.css'
-import '@/styles/main.css'
-import '@/styles/nav.css'
-import 'react-toastify/dist/ReactToastify.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { ToastContainer } from 'react-toastify'
+import { Noto_Serif_Display, Be_Vietnam_Pro } from 'next/font/google'
 
-import {GoogleAnalytics} from '@next/third-parties/google'
-import {ToastContainer} from "react-toastify";
-import {Noto_Serif_Display, Be_Vietnam_Pro} from 'next/font/google'
-
-
-const noto_serif_display = Noto_Serif_Display({subsets: ['latin'], variable: '--font-noto-serif-display'})
+const noto_serif_display = Noto_Serif_Display({
+	subsets: ['latin'],
+	variable: '--font-noto-serif-display',
+})
 const be_vietnam_pro = Be_Vietnam_Pro({
-        weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-        variable: '--font-be-vietnam-pro',
-        style: ['italic', 'normal'],
-        subsets: ['latin']
-    }
-)
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-be-vietnam-pro',
+	style: ['italic', 'normal'],
+	subsets: ['latin'],
+})
 
-export default function RootLayout({children, params}) {
-    return (
-        <html lang={`${params.lang}`}>
-        <body className={`relative text-slate-50 ${noto_serif_display.variable} ${be_vietnam_pro.variable}`}
-        >
-        <ToastContainer/>
-        <GoogleAnalytics gaId="UA-150969790-2"/>
-        <div className="gradient-bg">
-            <svg xmlns="http://www.w3.org/2000/svg" className={"gradient_svg"}>
-                <defs>
-                    <filter id="goo">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"/>
-                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo"/>
-                        <feBlend in="SourceGraphic" in2="goo"/>
-                    </filter>
-                </defs>
-            </svg>
-            <div className="gradients-container">
-                <div className="g1"></div>
-                <div className="g2"></div>
-                <div className="g3"></div>
-                <div className="g4"></div>
-                <div className="g5"></div>
-            </div>
-        </div>
-        {children}
-        </body>
-        </html>
-    )
+export default function RootLayout({ children, params }) {
+	return (
+		<html lang={`${params.lang}`}>
+			<body
+				className={`relative text-slate-50 ${noto_serif_display.variable} ${be_vietnam_pro.variable}`}
+			>
+				<ToastContainer />
+				<GoogleAnalytics gaId="UA-150969790-2" />
+				<div className="gradient-bg">
+					<svg xmlns="http://www.w3.org/2000/svg" className={'gradient_svg'}>
+						<defs>
+							<filter id="goo">
+								<feGaussianBlur
+									in="SourceGraphic"
+									stdDeviation="10"
+									result="blur"
+								/>
+								<feColorMatrix
+									in="blur"
+									mode="matrix"
+									values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+									result="goo"
+								/>
+								<feBlend in="SourceGraphic" in2="goo" />
+							</filter>
+						</defs>
+					</svg>
+					<div className="gradients-container">
+						<div className="g1"></div>
+						<div className="g2"></div>
+						<div className="g3"></div>
+						<div className="g4"></div>
+						<div className="g5"></div>
+					</div>
+				</div>
+				{children}
+			</body>
+		</html>
+	)
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,6 +4,21 @@ import { Layout } from '@/components/Global/Layout'
 import Footer from '@/components/Global/Footer'
 import { getContentWebsite, getNotFound } from '@/services/getContentWebsite'
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { ToastContainer } from 'react-toastify'
+import { Noto_Serif_Display, Be_Vietnam_Pro } from 'next/font/google'
+
+const noto_serif_display = Noto_Serif_Display({
+	subsets: ['latin'],
+	variable: '--font-noto-serif-display',
+})
+const be_vietnam_pro = Be_Vietnam_Pro({
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-be-vietnam-pro',
+	style: ['italic', 'normal'],
+	subsets: ['latin'],
+})
+
 export async function generateMetadata() {
 	// fetch data
 	let content_website = await getContentWebsite('en')
@@ -35,42 +50,77 @@ export default async function NotFound() {
 	notfound = notfound?.data
 
 	return (
-		<>
-			<div className="h-screen">
-				<div className="flex h-full items-center justify-center px-4 sm:px-6 lg:px-20 xl:px-24">
-					<div className="">
-						<Link href={'/'}>
-							<Image
-								alt="Logo Andy Cinquin"
-								width={50}
-								height={50}
-								src={`${process.env.NEXT_PUBLIC_URL}/assets/icons/logov2.svg`}
-							/>
-						</Link>
-						<div className={'mt-8'}>
-							<h1 className={'my-8 text-2xl font-semibold text-slate-50'}>
-								{content_website?.attributes?.content_notfound?.seo?.h1 ??
-									'404 Not Found'}
-							</h1>
-							<div className="mx-auto max-w-3xl">
-								<article>
-									<div className={'prose prose-invert my-8'}>
-										<Layout value={notfound?.attributes?.content.toString()} />
-									</div>
-								</article>
-							</div>
-
-							<Link
-								href={notfound?.attributes?.link?.url}
-								className="mt-8 text-slate-50 underline"
-							>
-								{notfound?.attributes?.link?.label}
+		<html lang={`en`}>
+			<body
+				className={`relative text-slate-50 ${noto_serif_display.variable} ${be_vietnam_pro.variable}`}
+			>
+				<ToastContainer />
+				<GoogleAnalytics gaId="UA-150969790-2" />
+				<div className="gradient-bg">
+					<svg xmlns="http://www.w3.org/2000/svg" className={'gradient_svg'}>
+						<defs>
+							<filter id="goo">
+								<feGaussianBlur
+									in="SourceGraphic"
+									stdDeviation="10"
+									result="blur"
+								/>
+								<feColorMatrix
+									in="blur"
+									mode="matrix"
+									values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+									result="goo"
+								/>
+								<feBlend in="SourceGraphic" in2="goo" />
+							</filter>
+						</defs>
+					</svg>
+					<div className="gradients-container">
+						<div className="g1"></div>
+						<div className="g2"></div>
+						<div className="g3"></div>
+						<div className="g4"></div>
+						<div className="g5"></div>
+					</div>
+				</div>
+				<div className="h-screen">
+					<div className="flex h-full items-center justify-center px-4 sm:px-6 lg:px-20 xl:px-24">
+						<div className="">
+							<Link href={'/'}>
+								<Image
+									alt="Logo Andy Cinquin"
+									width={50}
+									height={50}
+									src={`${process.env.NEXT_PUBLIC_URL}/assets/icons/logov2.svg`}
+								/>
 							</Link>
+							<div className={'mt-8'}>
+								<h1 className={'my-8 text-2xl font-semibold text-slate-50'}>
+									{content_website?.attributes?.content_notfound?.seo?.h1 ??
+										'404 Not Found'}
+								</h1>
+								<div className="mx-auto max-w-3xl">
+									<article>
+										<div className={'prose prose-invert my-8'}>
+											<Layout
+												value={notfound?.attributes?.content.toString()}
+											/>
+										</div>
+									</article>
+								</div>
+
+								<Link
+									href={notfound?.attributes?.link?.url}
+									className="mt-8 text-slate-50 underline"
+								>
+									{notfound?.attributes?.link?.label}
+								</Link>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<Footer content_website={content_website} />
-		</>
+				<Footer content_website={content_website} />
+			</body>
+		</html>
 	)
 }
