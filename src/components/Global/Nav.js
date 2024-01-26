3f5c36d4f3dcb9e645591268150725bfd5d73b29
CinquinAndy <cinquin.andy@gmail.com>
2024-01-26 11:33:47 +0100

    🔀 Simplify imports and remove unused code in Nav component
    This commit simplifies the imports in the Nav component by removing unused dependencies and optimizing the import statements. The changes include removing unnecessary useEffect, useRef, and useRouter imports.

--- src/components/Global/Nav.js
+++ src/components/Global/Nav.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
