22214c882e62eb37940470bc629e0f92ff1b42c6
CinquinAndy <cinquin.andy@gmail.com>
2024-01-26 12:26:42 +0100

    🔀 Update package-lock.json and package.json, 🌍 Modify Nav.js
    - Updated the version of "@next/third-parties" in package-lock.json to "14.1.0"
    - Modified the version of "next-i18n-router" in package.json to "^5.2.0"
    - In Nav.js, added imports for usePathname and useSearchParams from 'next/navigation'
    - Added a new parameter "locale" to the Nav component
    - Refactored switchLangage function in Nav.js to handle language switching based on the current pathname

--- src/components/Global/Nav.js
+++ src/components/Global/Nav.js
@@ -2,21 +2,22 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
-function Nav({ content_website, selectedMenu, h1, isHome = true }) {
+function Nav({ content_website, selectedMenu, h1, isHome = true, locale }) {
 	const [open, setOpen] = useState(false)
 	const router = useRouter()
-	const { pathname, asPath, query, locale } = router
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
 
 	content_website = content_website.attributes
 	const menu = content_website.menu
 	const socials = content_website.socials
 
 	const switchLangage = () => {
-		router.push({ pathname, query }, asPath, {
-			locale: locale === 'fr' ? 'en' : 'fr',
-		})
+		if (pathname) {
+			router.push(locale === 'fr' ? `${pathname}` : `/fr${pathname}`)
+		}
 	}
 
 	return (
