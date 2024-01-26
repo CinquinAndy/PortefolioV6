ba2816c81ac9044f49946a9a4e7ea2fe668636b3
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-26 20:37:08 +0100

    🚀 Add GalerySection component
    This commit adds a new file `GalerySection.js` to the `src/components/Global` directory. The `GalerySection` component is exported and includes several JSX elements and props. It also imports the `replaceTitle` function from the `@/services/utils` module.
    
    The component renders a container with various nested elements, including a title, button, and an embedded `<Galery>` component. The button has an onClick event handler that triggers the `handleClick()` function.
    
    No changes were made to existing files in this commit.

---
+++ src/components/Global/GalerySection.js
@@ -0,0 +1,47 @@
+import { replaceTitle } from '@/services/utils'
+
+export function GalerySection({ content_website, processedRealisation }) {
+	return (
+		<div
+			onClick={handleClick}
+			className={
+				'shadow-innercustom relative mx-auto max-w-5xl cursor-pointer p-8 md:col-span-2 md:p-20'
+			}
+		>
+			<div className={'flex w-full items-start gap-4 md:gap-8'}>
+				<div className={'flex items-center gap-2'}>
+					<h2
+						className={'text-md font-black md:text-3xl [&>*]:font-black'}
+						dangerouslySetInnerHTML={{
+							__html: replaceTitle(
+								content_website?.attributes?.content_realisations?.title_galery
+							),
+						}}
+					/>
+					<div className={'hidden h-full items-center md:flex'}>
+						<ChevronRightIcon className={'h-6 w-6 md:h-8 md:w-8'} />
+					</div>
+				</div>
+				<button
+					onClick={() => {
+						handleClick()
+					}}
+					className={
+						'custom-button-icons relative flex items-center gap-4 rounded border border-indigo-600 bg-transparent px-6 py-2 text-xs xl:px-8 xl:py-2 xl:text-sm'
+					}
+				>
+					{content_website?.attributes?.content_realisations?.btn_galery?.label}
+					<CameraIcon className={'absolute -right-2 -top-2 h-4 w-4 rotate-6'} />
+				</button>
+			</div>
+			<Galery
+				handleClick={handleClick}
+				open={open}
+				galery={realisations?.attributes?.galery?.data}
+				title_galery={
+					content_website?.attributes?.content_realisations?.title_galery
+				}
+			/>
+		</div>
+	)
+}
