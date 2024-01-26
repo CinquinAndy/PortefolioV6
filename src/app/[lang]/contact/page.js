5cdc96a2076a37a04572cb6481f466d63134f5eb
CinquinAndy <cinquin.andy@gmail.com>
2024-01-26 11:28:52 +0100

    🔧 Refactor contact form component
    - Moved the contact form logic to a separate function component
    - Imported necessary dependencies for form validation and toast notifications
    - Created a schema for form validation using zod library
    - Implemented form submission handling with async/await and fetch API
    - Added success and error toast notifications after form submission

--- src/app/[lang]/contact/page.js
+++ src/app/[lang]/contact/page.js
@@ -34,61 +34,6 @@ export default async function Page({ params }) {
 	let content_website = await getContentWebsite(params.lang)
 	content_website = content_website?.data
 
-	const schema = z
-		.object({
-			name: z.string().nonempty({
-				message: content_website?.attributes?.content_contact?.error_name,
-			}),
-			email: z.string().nonempty({
-				message: content_website?.attributes?.content_contact?.error_email,
-			}),
-			phone: z.string().nonempty({
-				message: content_website?.attributes?.content_contact?.error_phone,
-			}),
-			company: z.string(),
-			content: z.string().nonempty({
-				message: content_website?.attributes?.content_contact?.error_content,
-			}),
-		})
-		.required()
-
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset, // pour réinitialiser le formulaire
-	} = useForm({
-		resolver: zodResolver(schema),
-	})
-
-	// Créez une nouvelle fonction pour gérer la soumission du formulaire
-	const onSubmit = async data => {
-		const response = await fetch('/api/sendMail', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				name: data.name,
-				email: data.email,
-				phone: data.phone,
-				company: data.company,
-				content: data.content,
-			}),
-		})
-
-		if (response.ok) {
-			toast(content_website?.attributes?.content_contact?.toast_success, {
-				type: 'success',
-				icon: '✅',
-			})
-			reset()
-		} else {
-			toast(content_website?.attributes?.content_contact?.toast_error, {
-				type: 'error',
-				icon: '⛔',
-			})
-		}
-	}
-
 	return (
 		<>
 			<Nav
--- src/components/Global/ContactForm.js
+++ src/components/Global/ContactForm.js
@@ -1,4 +1,64 @@
-export function ContactForm(props) {
+'use client'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'react-toastify'
+
+export function ContactForm({ content_website }) {
+	const schema = z
+		.object({
+			name: z.string().nonempty({
+				message: content_website?.attributes?.content_contact?.error_name,
+			}),
+			email: z.string().nonempty({
+				message: content_website?.attributes?.content_contact?.error_email,
+			}),
+			phone: z.string().nonempty({
+				message: content_website?.attributes?.content_contact?.error_phone,
+			}),
+			company: z.string(),
+			content: z.string().nonempty({
+				message: content_website?.attributes?.content_contact?.error_content,
+			}),
+		})
+		.required()
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset, // pour réinitialiser le formulaire
+	} = useForm({
+		resolver: zodResolver(schema),
+	})
+
+	// Créez une nouvelle fonction pour gérer la soumission du formulaire
+	const onSubmit = async data => {
+		const response = await fetch('/api/sendMail', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				name: data.name,
+				email: data.email,
+				phone: data.phone,
+				company: data.company,
+				content: data.content,
+			}),
+		})
+
+		if (response.ok) {
+			toast(content_website?.attributes?.content_contact?.toast_success, {
+				type: 'success',
+				icon: '✅',
+			})
+			reset()
+		} else {
+			toast(content_website?.attributes?.content_contact?.toast_error, {
+				type: 'error',
+				icon: '⛔',
+			})
+		}
+	}
+
 	return (
 		<div>
 			<form
