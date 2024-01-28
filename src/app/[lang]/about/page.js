aa12c791d9a075d390ca7fdceb18a9a44f7da0b9
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-28 20:25:37 +0100

    🔧 Refactor code for about page
    - Import modules using single quotes instead of double quotes
    - Add import statement for HoloComponent
    - Reformat code to improve readability and consistency
    - Update function generateMetadata to return metadata for the about page
    - Update function Page to include HoloComponent and restructure layout

--- src/app/[lang]/about/page.js
+++ src/app/[lang]/about/page.js
@@ -1,59 +1,66 @@
-import {getAbout, getContentWebsite} from "@/services/getContentWebsite";
-import Nav from "@/components/Global/Nav";
-import Cta from "@/components/Global/Cta";
-import Footer from "@/components/Global/Footer";
-import {Layout} from "@/components/Global/Layout";
+import { getAbout, getContentWebsite } from '@/services/getContentWebsite'
+import Nav from '@/components/Global/Nav'
+import Cta from '@/components/Global/Cta'
+import Footer from '@/components/Global/Footer'
+import { Layout } from '@/components/Global/Layout'
+import { HoloComponent } from '@/app/[lang]/about/holoComponent'
 
-export async function generateMetadata({params}) {
-    // fetch data
-    const content_website = await getContentWebsite(params.lang)
+export async function generateMetadata({ params }) {
+	// fetch data
+	const content_website = await getContentWebsite(params.lang)
 
-    return {
-        title:
-            content_website?.data?.attributes?.content_about?.seo?.title ||
-            'Andy Cinquin - Freelance Entrepreneur & Developer',
-        description:
-            content_website?.data?.attributes?.content_about?.seo?.description ||
-            "Professional portfolio of Andy Cinquin, freelance software developer, Nantes and surrounding areas. Custom development, web, applications",
-        metadataBase: new URL(`https://andy-cinquin.com`),
-        alternates: {
-            canonical: content_website?.data?.attributes?.content_about?.seo?.canonical || '/',
-            languages: {
-                'en-US': '/',
-                'fr-FR': 'https://andy-cinquin.fr',
-            },
-        },
-    }
+	return {
+		title:
+			content_website?.data?.attributes?.content_about?.seo?.title ||
+			'Andy Cinquin - Freelance Entrepreneur & Developer',
+		description:
+			content_website?.data?.attributes?.content_about?.seo?.description ||
+			'Professional portfolio of Andy Cinquin, freelance software developer, Nantes and surrounding areas. Custom development, web, applications',
+		metadataBase: new URL(`https://andy-cinquin.com`),
+		alternates: {
+			canonical:
+				content_website?.data?.attributes?.content_about?.seo?.canonical || '/',
+			languages: {
+				'en-US': '/',
+				'fr-FR': 'https://andy-cinquin.fr',
+			},
+		},
+	}
 }
 
-export default async function Page({params}) {
-    let content_website = await getContentWebsite(params.lang)
-    content_website = content_website?.data
-    let about = await getAbout(params.lang)
-    about = about?.data
+export default async function Page({ params }) {
+	let content_website = await getContentWebsite(params.lang)
+	content_website = content_website?.data
+	let about = await getAbout(params.lang)
+	about = about?.data
 
-    return <>
-        <Nav
-            content_website={content_website}
-            isHome={false}
-            h1={content_website?.attributes?.content_about?.seo?.h1}
-        />
-        <div>
-            <div className={'relative'}>
-                <div className={'my-24 grid grid-cols-1 px-6 md:my-48 2xl:px-0'}>
-                    <div className="mx-auto max-w-3xl">
-                        <article>
-                            <div className={'prose prose-invert my-8'}>
-                                <Layout value={about?.attributes?.content.toString()} />
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div>
-        </div>
+	return (
+		<>
+			<Nav
+				content_website={content_website}
+				isHome={false}
+				h1={content_website?.attributes?.content_about?.seo?.h1}
+			/>
+			<div>
+				<div className={'relative'}>
+					<div className={'my-24 grid grid-cols-2 px-6 md:my-48 2xl:px-0'}>
+						<div className={'flex w-full justify-center'}>
+							<HoloComponent />
+						</div>
+						<div className="">
+							<article>
+								<div className={'prose prose-invert my-8'}>
+									<Layout value={about?.attributes?.content.toString()} />
+								</div>
+							</article>
+						</div>
+					</div>
+				</div>
+			</div>
 
-        <Cta content_website={content_website} />
+			<Cta content_website={content_website} />
 
-        <Footer content_website={content_website} />
-    </>
+			<Footer content_website={content_website} />
+		</>
+	)
 }
