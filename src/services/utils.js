fd9c661620a6acbf89cc458b2c1a446fb0f76608
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-30 20:30:04 +0100

    🔧 Refactor replaceTitle function to handle multiple text formats
    The commit refactors the replaceTitle function in utils.js to handle multiple text formats. It replaces asterisks with indigo color, underscores with sky color, and encapsulates the title in a span. The new implementation uses a regex to identify words wrapped in * or _, as well as other text segments. It iterates through the matches and applies the appropriate styling based on the match type.

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,17 +1,23 @@
 export const replaceTitle = title => {
-	console.log('title', title)
 	if (!title) return ''
-	// Replace asterisks with indigo color
-	const regexAsterisk = /\*([^*]+)\*/g
-	title = title.replace(
-		regexAsterisk,
-		'<span class="text-indigo-400">$1</span>'
-	)
 
-	// Replace underscores with sky color
-	const regexUnderscore = /_([^_]+)_/g
-	title = title.replace(regexUnderscore, '<span class="text-sky-400">$1</span>')
-	// encapsulate the title in a span
-	title = `<span>${title}</span>`
-	return title
+	// Regex to identify words with * or _, and other text segments
+	const regex = /(\*([^*]+)\*)|(_([^_]+)_)|([^*_]+)/g
+	let matches
+	let newTitle = ''
+
+	while ((matches = regex.exec(title)) !== null) {
+		if (matches[2]) {
+			// Match for text wrapped in *
+			newTitle += `<span class="text-indigo-400">${matches[2]}</span>`
+		} else if (matches[4]) {
+			// Match for text wrapped in _
+			newTitle += `<span class="text-sky-400">${matches[4]}</span>`
+		} else if (matches[5]) {
+			// Match for text without * or _
+			newTitle += `<span>${matches[5]}</span>`
+		}
+	}
+
+	return newTitle
 }
