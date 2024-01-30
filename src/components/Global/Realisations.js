a094e64914d53c31f71e6d1512e3603b63887a40
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-30 18:40:40 +0100

    🔧 Update Realisations component styling
    This commit updates the styling of the Realisations component in src/components/Global/Realisations.js. The className for the h2 element has been modified to include conditional styling based on screen size.

--- src/components/Global/Realisations.js
+++ src/components/Global/Realisations.js
@@ -22,7 +22,7 @@ function Realisations({ content_website, realisations, slice, isHome }) {
 				<div className="mt-[100px] flex justify-between">
 					<div className="w-1/2">
 						<h2
-							className="text-2xl normal-case leading-snug xl:text-5xl"
+							className="text-2xl normal-case leading-snug xl:text-5xl [&>*]:text-2xl xl:[&>*]:text-5xl"
 							dangerouslySetInnerHTML={{
 								__html: replaceTitle(
 									content_website?.attributes?.content_home?.title_realisation
