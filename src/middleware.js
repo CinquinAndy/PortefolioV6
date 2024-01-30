3cb94fea96a034026c529d774c59f6ba4d62a107
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-30 15:52:43 +0100

    Refactor middleware to set the accept-language header and remove unnecessary code
    The middleware function in `middleware.js` has been refactored to set the `accept-language` header based on the host of the request. Unnecessary code for redirecting has been removed.

--- src/middleware.js
+++ src/middleware.js
@@ -1,52 +1,20 @@
-// export function middleware(request) {
-// 	const locales = ['en', 'fr']
-// 	const { pathname, host } = request.nextUrl
-// 	let newLocale = ''
-//
-// 	// Determine the locale based on the domain
-// 	if (host === process.env.NEXT_PUBLIC_URL) {
-// 		newLocale = 'fr'
-// 	} else if (host === process.env.NEXT_PUBLIC_URL_ALT) {
-// 		newLocale = 'en'
-// 	} else {
-// 		newLocale = 'en' // Default locale for other domains
-// 	}
-//
-// 	// Check if the path contains a locale
-// 	const pathnameHasLocale = locales.some(locale => {
-// 		if (pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`) {
-// 			return true
-// 		}
-// 		return false
-// 	})
-//
-// 	// Redirect to [domain]/[locale] if it's the root
-// 	if (!pathnameHasLocale && pathname === '/') {
-// 		request.nextUrl.pathname = `/${newLocale}`
-// 		return Response.redirect(request.nextUrl)
-// 	}
-//
-// 	// Remove the locale segment from the URL if it exists
-// 	if (pathnameHasLocale) {
-// 		request.nextUrl.pathname = pathname.replace(`/${newLocale}`, '')
-// 		request.headers.set('accept-language', newLocale)
-// 		return Response.redirect(request.nextUrl)
-// 	}
-//
-// 	// No change if no locale is found in the URL
-// }
-//
-// export const config = {
-// 	matcher: '/((?!api|static|.*\\..*|_next).*)',
-// }
-
 export function middleware(request) {
-	const defaultLocale = 'en' // Utilisez votre locale par défaut
+	const { host } = request.nextUrl
 
-	if (request.nextUrl.pathname === '/') {
-		request.nextUrl.pathname = `/${defaultLocale}`
-		return Response.redirect(request.nextUrl)
+	let newLocale
+	if (host === process.env.NEXT_PUBLIC_URL) {
+		newLocale = 'fr'
+	} else if (host === process.env.NEXT_PUBLIC_URL_ALT) {
+		newLocale = 'en'
+	} else {
+		newLocale = 'en' // Locale par défaut pour les autres domaines
 	}
+
+	// Définir la locale dans les headers de la requête
+	request.headers.set('accept-language', newLocale)
+
+	// Utilisez le rendu conditionnel dans vos composants/pages pour afficher le contenu en fonction de la locale
+	// Pas de redirection nécessaire
 }
 
 export const config = {
