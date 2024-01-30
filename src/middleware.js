03da924ee4da9b2d476b698c859681f9ef2c4996
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-30 15:44:13 +0100

    Refactor middleware to set default locale and redirect root path
    The code changes refactor the middleware function to set a default locale and redirect the root path to include the default locale. This ensures that if no locale is specified in the URL, it will default to the specified value.

--- src/middleware.js
+++ src/middleware.js
@@ -1,39 +1,52 @@
+// export function middleware(request) {
+// 	const locales = ['en', 'fr']
+// 	const { pathname, host } = request.nextUrl
+// 	let newLocale = ''
+//
+// 	// Determine the locale based on the domain
+// 	if (host === process.env.NEXT_PUBLIC_URL) {
+// 		newLocale = 'fr'
+// 	} else if (host === process.env.NEXT_PUBLIC_URL_ALT) {
+// 		newLocale = 'en'
+// 	} else {
+// 		newLocale = 'en' // Default locale for other domains
+// 	}
+//
+// 	// Check if the path contains a locale
+// 	const pathnameHasLocale = locales.some(locale => {
+// 		if (pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`) {
+// 			return true
+// 		}
+// 		return false
+// 	})
+//
+// 	// Redirect to [domain]/[locale] if it's the root
+// 	if (!pathnameHasLocale && pathname === '/') {
+// 		request.nextUrl.pathname = `/${newLocale}`
+// 		return Response.redirect(request.nextUrl)
+// 	}
+//
+// 	// Remove the locale segment from the URL if it exists
+// 	if (pathnameHasLocale) {
+// 		request.nextUrl.pathname = pathname.replace(`/${newLocale}`, '')
+// 		request.headers.set('accept-language', newLocale)
+// 		return Response.redirect(request.nextUrl)
+// 	}
+//
+// 	// No change if no locale is found in the URL
+// }
+//
+// export const config = {
+// 	matcher: '/((?!api|static|.*\\..*|_next).*)',
+// }
+
 export function middleware(request) {
-	const locales = ['en', 'fr']
-	const { pathname, host } = request.nextUrl
-	let newLocale = ''
+	const defaultLocale = 'en' // Utilisez votre locale par défaut
 
-	// Determine the locale based on the domain
-	if (host === process.env.NEXT_PUBLIC_URL) {
-		newLocale = 'fr'
-	} else if (host === process.env.NEXT_PUBLIC_URL_ALT) {
-		newLocale = 'en'
-	} else {
-		newLocale = 'en' // Default locale for other domains
-	}
-
-	// Check if the path contains a locale
-	const pathnameHasLocale = locales.some(locale => {
-		if (pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`) {
-			return true
-		}
-		return false
-	})
-
-	// Redirect to [domain]/[locale] if it's the root
-	if (!pathnameHasLocale && pathname === '/') {
-		request.nextUrl.pathname = `/${newLocale}`
+	if (request.nextUrl.pathname === '/') {
+		request.nextUrl.pathname = `/${defaultLocale}`
 		return Response.redirect(request.nextUrl)
 	}
-
-	// Remove the locale segment from the URL if it exists
-	if (pathnameHasLocale) {
-		request.nextUrl.pathname = pathname.replace(`/${newLocale}`, '')
-		request.headers.set('accept-language', newLocale)
-		return Response.redirect(request.nextUrl)
-	}
-
-	// No change if no locale is found in the URL
 }
 
 export const config = {
