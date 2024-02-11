b83a174098c42c4756b423c858d65a701654bbbe
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-11 22:03:39 +0100

    🔧 Add BackButton component for portfolio images
    This commit adds the `BackButton` component to the portfolio image page. The component includes a button with a backward icon that allows users to navigate back to the previous page using Next.js router.

---
+++ src/app/[locale]/portefolio/[slug]/[image]/backButton.js
@@ -0,0 +1,18 @@
+'use client'
+import { BackwardIcon } from '@heroicons/react/20/solid'
+import { useRouter } from 'next/navigation'
+
+export function BackButton() {
+	const router = useRouter()
+
+	return (
+		<button
+			type="button"
+			className="rounded-full rounded-md border-2 border-white bg-transparent p-3 text-slate-100 hover:text-slate-200"
+			onClick={() => router.back()}
+		>
+			<span className="sr-only">Back</span>
+			<BackwardIcon className="h-6 w-6" aria-hidden="true" />
+		</button>
+	)
+}
