83860efbfe27604175befa496072ce4a74992828
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-11 22:03:45 +0100

    🔧 Improve image loading functionality
    This commit improves the image loading functionality by adding a loading spinner while the image is being loaded. It also updates the component to use React's useState hook for managing the loaded state.

--- src/app/[locale]/portefolio/[slug]/[image]/imageLoad.js
+++ src/app/[locale]/portefolio/[slug]/[image]/imageLoad.js
@@ -1,14 +1,34 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
 
-export function ImageLoad({ image }) {
+export function ImageLoad({ className, src, alt, width, height }) {
+	const [loaded, setLoaded] = useState(false)
+
 	return (
-		<Image
-			src={image?.attributes?.url}
-			alt={image?.attributes?.alternativeText ?? 'Project Image'}
-			width={image?.attributes?.width}
-			height={image?.attributes?.height}
-			className={`rounded-lg p-32`}
-			quality={75}
-		/>
+		<>
+			{!loaded && (
+				<div
+					className={
+						'absolute left-0 top-0 flex h-full w-full items-center justify-center'
+					}
+				>
+					<div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-100 motion-reduce:animate-[spin_1.5s_linear_infinite]">
+						<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+							Loading...
+						</span>
+					</div>
+				</div>
+			)}
+			<Image
+				src={src}
+				alt={alt}
+				width={width}
+				height={height}
+				className={`${loaded ? 'block' : 'fixed -z-10 opacity-0'} ${className}`}
+				quality={75}
+				onLoad={() => setLoaded(true)}
+			/>
+		</>
 	)
 }
