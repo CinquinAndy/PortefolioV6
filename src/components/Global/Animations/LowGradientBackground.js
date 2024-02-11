5baaa83b2a1bfebefcff8fbe4302a78e60c78f4f
Cinquin Andy <andy.cinquin@infotel.com>
2024-02-11 22:03:54 +0100

    🔧 Fix LowGradientBackground.js
    Remove unused gradient divs in LowGradientBackground component.
    
    The code changes remove the unused "g2" and "g4" gradient divs from the LowGradientBackground component. These divs were commented out to improve code readability and reduce unnecessary rendering.

--- src/components/Global/Animations/LowGradientBackground.js
+++ src/components/Global/Animations/LowGradientBackground.js
@@ -3,9 +3,9 @@ export function LowGradientBackground() {
 		<div className={`gradient-bg`}>
 			<div className="gradients-container">
 				<div className="g1"></div>
-				<div className="g2"></div>
+				{/*<div className="g2"></div>*/}
 				<div className="g3"></div>
-				<div className="g4"></div>
+				{/*<div className="g4"></div>*/}
 				<div className="g5"></div>
 			</div>
 		</div>
