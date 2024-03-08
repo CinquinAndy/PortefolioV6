790cd0fb134dfcb0e3c0a726e712dfbdeec52dd3
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-08 18:42:51 +0100

    ✨ Add function to generate CSS variables for colors
    Added a new function to generate CSS variables for colors in the Tailwind config file. This function extracts color values and creates corresponding CSS variables for them, enhancing customization options.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+const {
+	default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette')
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -47,5 +51,20 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		addVariablesForColors,
+	],
+}
+
+function addVariablesForColors({ addBase, theme }) {
+	let allColors = flattenColorPalette(theme('colors'))
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	)
+
+	addBase({
+		':root': newVars,
+	})
 }
