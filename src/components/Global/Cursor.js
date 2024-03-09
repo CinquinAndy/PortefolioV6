6679cb77a2bc8758e1e1561458ed00d99f4d2680
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-09 19:35:07 +0100

    ✨ Add cursor functionality & styles
    - Added state management for cursor visibility and size
    - Implemented logic to update cursor position on mouse movement
    - Refactored event listeners for better performance

--- src/components/Global/Cursor.js
+++ src/components/Global/Cursor.js
@@ -1,140 +1,151 @@
 'use client'
-import { Suspense, useEffect } from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
 
-function Cursor() {
+// // Custom hook to detect path changes
+// function usePathChangeEffect(effect, deps = []) {
+// 	const pathname = usePathname()
+//
+// 	useEffect(effect, [pathname, ...deps])
+// }
+
+// function Cursor() {
+// usePathChangeEffect(() => {
+// 	// Logic to reset your cursor here
+// 	// For example, resetting colors or CSS classes based on page state
+// 	const cursor = document.querySelector('.cursor')
+// 	if (cursor) {
+// 		cursor.classList.remove('expand', 'hover')
+// 		// Reset other states or styles as necessary
+// 	}
+// }, [])
+//
+// useEffect(() => {
+// 	const style = document.createElement('style')
+// 	style.innerHTML = `
+//
+// 	  `
+// 	document.head.appendChild(style)
+//
+// 	let cursor = document.querySelector('.cursor')
+// 	if (!cursor) {
+// 		cursor = document.createElement('span')
+// 		cursor.className = 'cursor'
+// 		document.body.appendChild(cursor)
+// 	}
+//
+// 	const elements = document.querySelectorAll(
+// 		'a, button, .cursor-pointer, .custom-button-icons, .Toastify'
+// 	)
+// 	elements.forEach(element => {
+// 		element.addEventListener('mouseenter', () => {
+// 			cursor.classList.add('hover')
+// 		})
+// 		element.addEventListener('mouseleave', () => {
+// 			cursor.classList.remove('hover')
+// 		})
+// 	})
+//
+// 	document.addEventListener('click', () => {
+// 		cursor.classList.add('expand')
+//
+// 		setTimeout(() => {
+// 			cursor.classList.remove('expand')
+// 		}, 500)
+// 	})
+//
+// 	let isMouseDown = false
+//
+// 	document.addEventListener('mousedown', () => {
+// 		isMouseDown = true
+// 		setTimeout(() => {
+// 			if (isMouseDown) {
+// 				cursor.classList.add('expand')
+// 			}
+// 		}, 100)
+// 	})
+//
+// 	document.addEventListener('mouseup', () => {
+// 		isMouseDown = false
+// 		cursor.classList.remove('expand')
+// 	})
+//
+// 	let lastX = 0
+// 	let lastY = 0
+// 	let cursorX = 0
+// 	let cursorY = 0
+// 	const speed = 0.5 // Adjust speed for smoother cursor movement
+//
+// 	function animate() {
+// 		const dx = (lastX - cursorX) * speed
+// 		const dy = (lastY - cursorY) * speed
+//
+// 		cursorX += dx
+// 		cursorY += dy
+//
+// 		cursor.style.transform = `translate3d(${cursorX}px, ${cursorY}px, 0)`
+// 		requestAnimationFrame(animate)
+// 	}
+//
+// 	document.addEventListener('mousemove', e => {
+// 		lastX = e.clientX
+// 		lastY = e.clientY
+// 	})
+//
+// 	animate()
+// }, [])
+const Cursor = () => {
+	const [cursorVisible, setCursorVisible] = useState(true)
+	const [cursorEnlarged, setCursorEnlarged] = useState(false)
+	const [position, setPosition] = useState({ x: 0, y: 0 })
+
 	useEffect(() => {
-		const style = document.createElement('style')
-		style.innerHTML = `
-			body {
-			  	cursor: none;
-			}
-			
-			*:hover {
-			  	cursor: none !important;
-			}
-			
-			:root {
-    			--cursor-before-bg-color: #fff;
-			}
-
-			.cursor {
-				position: fixed;
-				width: 30px;
-				height: 30px;
-				/*border from the circle*/
-				background: rgba(0,0,0,0.7);
-				border-radius: 50%;
-				top: var(--y, 0);
-				left: var(--x, 0);
-				transform: translate(-50%, -50%);
-				mix-blend-mode: multiply;
-				z-index: 999999;
-				pointer-events: none;
-				transition: transform 0.2s ease-out, background-color 0.2s ease;
-			}
-			
-			.cursor::before {
-				content: '';
-				position: fixed;
-				width: 27px;
-				height: 27px;
-				background-color: var(--cursor-before-bg-color);
-				border-radius: 50%;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-				pointer-events: none;
-				transition: transform 0.2s ease-out, background-color 0.2s ease;
-			}
-			
-			.cursor.hover {
-				transform: translate(-50%, -50%) scale(1.4);
-			}
-			
-			.cursor.expand{
-				transform: translate(-50%, -50%) scale(1.4);
-			}
-		  `
-		document.head.appendChild(style)
-		const cursor = document.querySelector('.cursor')
-
-		const changeCursorColor = color => {
-			document.documentElement.style.setProperty(
-				'--cursor-before-bg-color',
-				color
-			)
+		const updateCursor = e => {
+			setPosition({ x: e.clientX, y: e.clientY })
 		}
 
-		const changeCursorBorderColor = color => {
-			cursor.style.backgroundColor = color
-		}
+		const toggleCursorVisibility = isVisible => setCursorVisible(isVisible)
+		const toggleCursorSize = isEnlarged => setCursorEnlarged(isEnlarged)
+
+		window.addEventListener('mousemove', updateCursor)
+		window.addEventListener('mouseenter', () => toggleCursorVisibility(true))
+		window.addEventListener('mouseleave', () => toggleCursorVisibility(false))
+		window.addEventListener('mousedown', () => toggleCursorSize(true))
+		window.addEventListener('mouseup', () => toggleCursorSize(false))
 
 		document
 			.querySelectorAll(
-				'a, button, .cursor-pointer,.custom-button-icons, .Toastify'
+				'a, button, .cursor-pointer, .custom-button-icons, .Toastify'
 			)
-			.forEach(element => {
-				element.addEventListener('mouseenter', () => {
-					cursor.classList.add('hover')
-					changeCursorColor('#919191')
-					changeCursorBorderColor('rgba(0, 0, 0, 0.8)')
-				})
-				element.addEventListener('mouseleave', () => {
-					cursor.classList.remove('hover')
-					changeCursorColor('#ccc')
-					changeCursorBorderColor('rgba(0,0,0,0.7)')
-				})
+			.forEach(el => {
+				el.addEventListener('mouseenter', () => toggleCursorSize(true))
+				el.addEventListener('mouseleave', () => toggleCursorSize(false))
 			})
 
-		document.addEventListener('click', () => {
-			cursor.classList.add('expand')
-			changeCursorColor('#919191')
-			changeCursorBorderColor('rgba(0, 0, 0, 0.8)')
-
-			setTimeout(() => {
-				cursor.classList.remove('expand')
-				changeCursorColor('#ccc')
-				changeCursorBorderColor('rgba(0,0,0,0.7)')
-			}, 500)
-		})
-
-		let isMouseDown = false
-
-		document.addEventListener('mousedown', () => {
-			isMouseDown = true
-			setTimeout(() => {
-				if (isMouseDown) {
-					cursor.classList.add('expand')
-					changeCursorColor('#919191')
-					changeCursorBorderColor('rgba(0, 0, 0, 0.8)')
-				}
-			}, 100)
-		})
-
-		document.addEventListener('mouseup', () => {
-			isMouseDown = false
-			cursor.classList.remove('expand')
-			changeCursorColor('#ccc')
-			changeCursorBorderColor('rgba(0,0,0,0.7)')
-		})
-
-		let lastX = 0
-		let lastY = 0
-
-		document.addEventListener('mousemove', e => {
-			lastX = e.clientX
-			lastY = e.clientY
-			requestAnimationFrame(() => {
-				cursor.style.left = `${lastX}px`
-				cursor.style.top = `${lastY}px`
-			})
-		})
+		return () => {
+			window.removeEventListener('mousemove', updateCursor)
+			window.removeEventListener('mouseenter', () =>
+				toggleCursorVisibility(true)
+			)
+			window.removeEventListener('mouseleave', () =>
+				toggleCursorVisibility(false)
+			)
+			window.removeEventListener('mousedown', () => toggleCursorSize(true))
+			window.removeEventListener('mouseup', () => toggleCursorSize(false))
+		}
 	}, [])
 
 	return (
-		<Suspense>
-			<span className="cursor"></span>
-		</Suspense>
+		<>
+			<div
+				className={`pointer-events-none fixed z-50 h-2 w-2 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white transition-transform duration-300 ease-in-out ${cursorEnlarged ? 'scale-90' : 'scale-100'}`}
+				style={{ left: `${position.x}px`, top: `${position.y}px` }}
+			></div>
+			<div
+				className={`pointer-events-none fixed z-50 h-10 w-10 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white/10 transition-transform duration-300 ease-in-out ${cursorEnlarged ? 'scale-150' : 'scale-100'}`}
+				style={{ left: `${position.x}px`, top: `${position.y}px` }}
+			></div>
+		</>
 	)
 }
 
