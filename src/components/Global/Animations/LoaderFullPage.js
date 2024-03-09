cdaccb8f0ea00275a932b97630998cd228ca2299
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-09 02:55:26 +0100

    🛠️ Refactor loader display logic and structure
    Refactored the loader component to improve conditional rendering and added a new background beams component for enhanced visual effect.

--- src/components/Global/Animations/LoaderFullPage.js
+++ src/components/Global/Animations/LoaderFullPage.js
@@ -16,31 +16,39 @@ function LoaderFullPage({ params }) {
 		}, 7000)
 	}, [])
 	return (
-		<div
-			className={`${loading === false ? 'pointer-events-none opacity-0' : 'opacity-100'} body-loader 
-			fixed left-0 top-0 z-[999] h-dvh w-dvw flex-col items-center justify-center antialiased 
-			transition-opacity duration-1000 ${hide === true ? 'hidden' : 'flex'}`}
-		>
-			<div className="mx-auto max-w-2xl p-4">
-				<div className="scale-125 transform">
-					<div className="loader-circle"></div>
-					<div className="loader-line-mask">
-						<div className="loader-line" />
+		<>
+			{hide === true ? (
+				<div className="hidden" />
+			) : (
+				<div
+					className={`${loading === false ? 'pointer-events-none opacity-0' : 'opacity-100'} body-loader 
+			fixed left-0 top-0 z-[999] flex h-dvh w-dvw flex-col items-center justify-center 
+			antialiased transition-opacity duration-1000`}
+				>
+					<div className="mx-auto max-w-2xl p-4">
+						<div className="scale-125 transform">
+							<div className="loader-circle"></div>
+							<div className="loader-line-mask">
+								<div className="loader-line" />
+							</div>
+							<span
+								className={
+									'z-10 flex h-full w-full items-center justify-center'
+								}
+							>
+								<Image
+									src={'assets/icons/logov2.svg'}
+									width={50}
+									height={50}
+									alt={'icon'}
+								/>
+							</span>
+						</div>
 					</div>
-					<span
-						className={'z-10 flex h-full w-full items-center justify-center'}
-					>
-						<Image
-							src={'assets/icons/logov2.svg'}
-							width={50}
-							height={50}
-							alt={'icon'}
-						/>
-					</span>
+					<BackgroundBeams />
 				</div>
-			</div>
-			<BackgroundBeams />
-		</div>
+			)}
+		</>
 	)
 }
 
