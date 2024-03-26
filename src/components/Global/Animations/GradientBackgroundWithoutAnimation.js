deb37201eefb6c2f6b5dc63dfa78d42ce365aa38
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-26 15:54:57 +0100

    ✨ Update component name and remove unused gradient elements
    Changed the component name to better reflect its purpose. Removed unused gradient elements for optimization.

--- src/components/Global/Animations/GradientBackgroundWithoutAnimation.js
+++ src/components/Global/Animations/GradientBackgroundWithoutAnimation.js
@@ -1,12 +1,12 @@
-export function LowGradientBackground() {
+export function GradientBackgroundWithoutAnimation() {
 	return (
 		<div className={`gradient-bg`}>
 			<div className="gradients-container">
-				<div className="g1"></div>
+				{/*<div className="g1"></div>*/}
 				{/*<div className="g2"></div>*/}
 				{/*<div className="g3"></div>*/}
 				{/*<div className="g4"></div>*/}
-				<div className="g5"></div>
+				{/*<div className="g5"></div>*/}
 			</div>
 		</div>
 	)
